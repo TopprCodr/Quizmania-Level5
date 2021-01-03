de015a8ebcb01b81cf4347fc7909aeb84d3e0989
TopprCodr <72185815+TopprCodr@users.noreply.github.com>
2021-01-03 14:38:06 +0530

    Add files via upload

---
+++ src/screens/QuizDetails.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+export default function QuizDetails() {
+ 
+    //component rendering
+    return (
+       <Text>show Quiz Details</Text>
+    );
+}
